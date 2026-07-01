f21f59345ffefd9822254401c14767463cce52ae
Eren Tural <eren.tural61@gmail.com>
2026-07-01 09:44:28 +0300

    site yönlendirmeleri güncellendi

--- enerji-sistemi/app/layout.js
+++ enerji-sistemi/app/layout.js
@@ -2,9 +2,9 @@ import { Inter } from 'next/font/google'
 import './globals.css'
 import { AuthProvider } from '@/components/AuthProvider'
 import { ThemeProvider } from '@/components/ThemeProvider'
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const inter = Inter({ subsets: ['latin'] })
-
 
 export const metadata = {
   title: 'Enerji Sistemi',
@@ -21,9 +21,12 @@ export default function RootLayout({ children }) {
             {children}
           </ThemeProvider>
         </AuthProvider>
+        
+        {/* VERCEL SPEED INSIGHTS BİLEŞENİ BURAYA EKLENDİ */}
+        <SpeedInsights />
+        
         <meta name="google-site-verification" content="Sp24FlwX9QmeamUO1MPQId3BZovoM4-oh8m0Lg7Y9VI" />
       </body>
     </html>
   )
 }
-
